0cbe23c4752381e056271e90de4dcabe4d92a610
DineshVetal <vetaldinu@gmail.com>
2021-04-03 19:52:45 +0530

    db connection added

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,19 @@ const db = require('./queries')
 // });
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '1234',
+  user: 'tlyecqezatjcgb',
+  host: 'ec2-52-44-31-100.compute-1.amazonaws.com',
+  database: 'd77d7pkvqo39mr',
+  password: '96ec013dbe30e7b521fb4cfb507eeb02289ebf578254c3592e0581e94e282d21',
   port: 5432,
 })
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: '1234',
+//   port: 5432,
+// })
 
 router.get('/', function(req, res, next){
    pool.query('SELECT question_id, question, options, answer,o.id as answer_id FROM public."tbl_Questions" as q join public."tbl_Options" as o on q.id=o.question_id', (error, results) => {
