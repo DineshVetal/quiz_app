c34dd3e766c9abcecb79bc7ac46694cfbfdcf9f7
DineshVetal <vetaldinu@gmail.com>
2021-04-03 20:15:34 +0530

    url added

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,17 @@ var express = require('express');
 var router = express.Router();
 const db = require('./queries')
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'tlyecqezatjcgb',
-  host: 'ec2-52-44-31-100.compute-1.amazonaws.com',
-  database: 'd77d7pkvqo39mr',
-  password: '96ec013dbe30e7b521fb4cfb507eeb02289ebf578254c3592e0581e94e282d21',
-  port: 5432,
-})
+
+// const Pool = require('pg')
+// const pool = new Pool({
+//   user: 'tlyecqezatjcgb',
+//   host: 'ec2-52-44-31-100.compute-1.amazonaws.com',
+//   database: 'd77d7pkvqo39mr',
+//   password: '96ec013dbe30e7b521fb4cfb507eeb02289ebf578254c3592e0581e94e282d21',
+//   port: 5432,
+// })
+
+// const Pool = require('pg').Pool
 // const pool = new Pool({
 //   user: 'postgres',
 //   host: 'localhost',
@@ -22,8 +21,19 @@ const pool = new Pool({
 //   port: 5432,
 // })
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString:'postgres://tlyecqezatjcgb:96ec013dbe30e7b521fb4cfb507eeb02289ebf578254c3592e0581e94e282d21@ec2-52-44-31-100.compute-1.amazonaws.com:5432/d77d7pkvqo39mr',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
 router.get('/', function(req, res, next){
-   pool.query('SELECT question_id, question, options, answer,o.id as answer_id FROM public."tbl_Questions" as q join public."tbl_Options" as o on q.id=o.question_id', (error, results) => {
+  client.query('SELECT question_id, question, options, answer,o.id as answer_id FROM public."tbl_Questions" as q join public."tbl_Options" as o on q.id=o.question_id', (error, results) => {
     if (error) {
       throw error
     }
@@ -53,6 +63,7 @@ router.get('/', function(req, res, next){
     console.log(data)
 
   res.render('home', { title: 'Questions' , data});
+  client.end()
   })
   
 });
